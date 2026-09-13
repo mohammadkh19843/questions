ffbe56734d01ccb3a16aa587480bbaeea67665c1
mohammadkh19843 <153128194+mohammadkh19843@users.noreply.github.com>
2026-09-13 19:52:30 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,25 @@
+import json
+
 from telebot import TeleBot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from telebot.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    WebAppInfo,
+)
 
 from config import BOT_TOKEN, WEBAPP_URL
+from database import init_db
+
 
 bot = TeleBot(BOT_TOKEN)
+
+init_db()
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
     keyboard = InlineKeyboardMarkup()
+
     keyboard.add(
         InlineKeyboardButton(
             "🗳️ ساخت سؤال",
@@ -21,6 +32,15 @@ def start(message):
         "🗳️ Questions\n\n"
         "از اینجا می‌توانی سؤال، گزینه‌ها و Quiz خودت را بسازی.",
         reply_markup=keyboard,
+    )
+
+
+@bot.message_handler(commands=["id"])
+def get_chat_id(message):
+    bot.send_message(
+        message.chat.id,
+        f"Chat ID:\n`{message.chat.id}`",
+        parse_mode="Markdown",
     )
 
 
